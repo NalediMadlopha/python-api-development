64703f6af1eb7a888ef4a5bfc5a91168b30d21e3
= <=>
2022-11-11 12:38:50 +0200

    - Move all sensetive setting in to a .env file. - Use environment variables for confidential settings

--- src/database.py
+++ src/database.py
@@ -1,11 +1,14 @@
-import time
-import psycopg2
-from psycopg2.extras import RealDictCursor
+# import time
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pwd123@127.0.0.1/fastapi"
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}' \
+                          f'@{settings.database_hostname}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
